e2559643f41f6124e6b4042730dee7bd31a0a7b3
mayandra.vmota <mayandra.vmota@SBC0505247W10-1>
2022-05-13 15:56:23 -0300

    Adicionar arquivos de projeto.

---
+++ onze/View/main.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace onze.View
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.ltbLeitor = new System.Windows.Forms.ListBox();
+            this.btnLer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(54, 60);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(245, 15);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Leitor de Texto com Informações Delimitadas";
+            // 
+            // ltbLeitor
+            // 
+            this.ltbLeitor.FormattingEnabled = true;
+            this.ltbLeitor.ItemHeight = 15;
+            this.ltbLeitor.Location = new System.Drawing.Point(54, 125);
+            this.ltbLeitor.Name = "ltbLeitor";
+            this.ltbLeitor.Size = new System.Drawing.Size(245, 169);
+            this.ltbLeitor.TabIndex = 1;
+            // 
+            // btnLer
+            // 
+            this.btnLer.Location = new System.Drawing.Point(224, 94);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(75, 23);
+            this.btnLer.TabIndex = 2;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = true;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLer);
+            this.Controls.Add(this.ltbLeitor);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "main";
+            this.Text = "main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox ltbLeitor;
+        private System.Windows.Forms.Button btnLer;
+    }
+}
---
+++ onze/View/main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace onze.View
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+            string endereco = @"C:\Senac\teste007.txt";
+            try
+            {
+                if (!System.IO.File.Exists(endereco))
+                {
+                    throw new Exception("Arquivo teste007.txt não foi localizado.");
+                }
+            string[] dados = new string[4];
+
+
+         System.IO.StreamReader Leitor = new System.IO.StreamReader(endereco);
+
+                ltbLeitor.Items.Add("Código".PadRight(7) +
+                "Cliente".PadRight(40) +
+                "Cidade".PadRight(20) +
+                "País"
+                );
+                ltbLeitor.Items.Add(new string('-', 80));
+
+                while (!Leitor.EndOfStream)
+                {
+                    dados = Leitor.ReadLine().Split(';');
+                    ltbLeitor.Items.Add(dados[0].PadRight(7) +
+                                        dados[1].PadRight(40) +
+                                        dados[2].PadRight(20) +
+                                        dados[3]);
+                }
+                Leitor.Close();
+
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+    }
+    }
+
